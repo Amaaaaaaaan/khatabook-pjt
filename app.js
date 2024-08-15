e5ce980d71826317e18ba159a6181bc3a108b7c0
Aman Tomar <amantoma700@gmail.com>
2024-08-15 16:23:37 +0530

    added some new stuffg

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use("/", indexRouter);
 app.use("/hisaab", hisaabRouter);
 
 // Start server
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
