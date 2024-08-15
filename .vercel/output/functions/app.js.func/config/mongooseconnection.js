893a5981689c05fa95f9ebff6fb6018511bbf068
user.name.like Aman Tomar <user.email.like amantoma700@gmail.com>
2024-08-15 17:33:04 +0530

    issues fixed

--- .vercel/output/functions/app.js.func/config/mongooseconnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_DB_URI).then(function(){
-    console.log("connected to db");
-})
-
-let db = mongoose.connection;
-
-module.exports = db;
--- .vercel/output/functions/app.js.func/config/multer-config.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const multer = require('multer');
-const storage = multer.memoryStorage()
-module.exports = multer({ storage: storage })
